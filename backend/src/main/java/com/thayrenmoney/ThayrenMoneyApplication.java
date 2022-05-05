0c9cad84a402f080a52a4ed6ee3994385610543d
Renan Duarte <renandg.axl@gmail.com>
2022-05-04 21:17:21 -0300

    Project created and initial configurations

---
+++ backend/src/main/java/com/thayrenmoney/ThayrenMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.thayrenmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThayrenMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThayrenMoneyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thayrenmoney/ThayrenMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayrenmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThayrenMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
